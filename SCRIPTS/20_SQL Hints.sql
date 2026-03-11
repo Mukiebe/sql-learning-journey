424b7a65a82f60a21b681b6a239cdf59126ba1c0
Mukiebe Maryann <mukiebemaryann22@gmail.com>
2026-03-11 08:48:03 +0300

    Add files via upload

---
+++ SCRIPTS/20_SQL Hints.sql
@@ -0,0 +1,29 @@
+SELECT 
+	o.Sales,
+	c.Country
+FROM Sales.Orders o
+LEFT JOIN Sales.Customers c
+ON o.CustomerID = c.CustomerID
+
+SELECT 
+	o.Sales,
+	c.Country
+FROM Sales.Orders o
+LEFT JOIN Sales.Customers c
+ON o.CustomerID = c.CustomerID
+OPTION (HASH JOIN)
+
+SELECT 
+	o.Sales,
+	c.Country
+FROM Sales.Orders o
+LEFT JOIN Sales.Customers c WITH (FORCESEEK)
+ON o.CustomerID = c.CustomerID
+
+
+SELECT 
+	o.Sales,
+	c.Country
+FROM Sales.Orders o
+LEFT JOIN Sales.Customers c WITH (INDEX ([PK_Customer_A4AE64B87FC20A48]))
+ON o.CustomerID = c.CustomerID
